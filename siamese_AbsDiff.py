4af0912e928d56723894d9f4885aad5f4392a47c
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-04-10 01:20:36 +0900

    Update siamese_AbsDiff.py

--- siamese_AbsDiff.py
+++ siamese_AbsDiff.py
@@ -27,7 +27,7 @@ class_1_accuracies = []
 
 original_dir = '/Dataset/dataset_patch_raw_ver3/original'
 denoised_dir = '/Dataset/dataset_patch_raw_ver3/denoised'
-csv_path     = '/Dataset/patch_label_median.csv'
+csv_path     = '/Dataset/Data_Aug_GA/patch_label_median_verified.csv'
 result_file_path = "/Dataset/Results/Overall_results.csv"
 
 
