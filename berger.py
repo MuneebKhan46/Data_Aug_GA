f0b5f32e0e2b860d46d77e6d86648075e5f1519c
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-04-11 17:14:16 +0900

    Update berger.py

--- berger.py
+++ berger.py
@@ -156,10 +156,19 @@ count = cv2.countNonZero(detected_image)
 print('Number of ghosting patches = {}, {:.2f}%'.format(count, 100 * count / (720 * 1280 / (d * d))))
 
 ghost_image = np.stack([fused_image]*3, axis=-1)  # Convert grayscale back to RGB for visualization
-for y in range((720 // d) * d):
-    for x in range((1280 // d) * d):
-        if detected_image[y // d][x // d] == 1:
+
+
+height = fused_image.shape[0]
+width = fused_image.shape[1]
+for y in range((height // d) * d):
+    for x in range((width // d) * d):
+        if detected_image[min(y // d, mask_height - 1)][min(x // d, mask_width - 1)] == 1:
             ghost_image[y, x] = (0, 0, 255)
+
+# for y in range((720 // d) * d):
+#     for x in range((1280 // d) * d):
+#         if detected_image[y // d][x // d] == 1:
+#             ghost_image[y, x] = (0, 0, 255)
 
 
 
