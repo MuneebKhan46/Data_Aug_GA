66e0744fdb150ead83ab0ae24003e752085ad452
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-05-14 11:26:29 +0900

    Create AutoKeras.py

---
+++ AutoKeras.py
@@ -0,0 +1,210 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import autokeras as ak
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle as sklearn_shuffle
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+models = []
+class_1_accuracies = []
+
+original_dir = '/Dataset/dataset_patch_raw_ver3/original'
+denoised_dir = '/Dataset/dataset_patch_raw_ver3/denoised'
+csv_path     = '/Dataset/Data_Aug_GA/patch_label_median_verified2.csv'
+result_file_path = "/Dataset/Results/Overall_results.csv"
+
+def extract_y_channel_from_yuv_with_patch_numbers(yuv_file_path: str, width: int, height: int):
+    y_size = width * height
+    patches, patch_numbers = [], []
+
+    if not os.path.exists(yuv_file_path):
+        print(f"Warning: File {yuv_file_path} does not exist.")
+        return [], []
+
+    with open(yuv_file_path, 'rb') as f:
+        y_data = f.read(y_size)
+
+    if len(y_data) != y_size:
+        print(f"Warning: Expected {y_size} bytes, got {len(y_data)} bytes.")
+        return [], []
+
+    y_channel = np.frombuffer(y_data, dtype=np.uint8).reshape((height, width))
+    patch_number = 0
+
+    for i in range(0, height, 224):
+        for j in range(0, width, 224):
+            patch = y_channel[i:i+224, j:j+224]
+            if patch.shape[0] < 224 or patch.shape[1] < 224:
+                patch = np.pad(patch, ((0, 224 - patch.shape[0]), (0, 224 - patch.shape[1])), 'constant')
+            patches.append(patch)
+            patch_numbers.append(patch_number)
+            patch_number += 1
+
+    return patches, patch_numbers
+
+def load_data_from_csv(csv_path, original_dir, denoised_dir):
+    df = pd.read_csv(csv_path)
+    
+    all_original_patches = []
+    all_denoised_patches = []
+    all_scores = []
+    denoised_image_names = []
+    all_patch_numbers = []
+
+    for _, row in df.iterrows():
+        original_file_name = f"original_{row['original_image_name']}.raw"
+        denoised_file_name = f"denoised_{row['original_image_name']}.raw"
+
+        original_path = os.path.join(original_dir, original_file_name)
+        denoised_path = os.path.join(denoised_dir, denoised_file_name)
+        
+        original_patches, original_patch_numbers = extract_y_channel_from_yuv_with_patch_numbers(original_path, row['width'], row['height'])
+        denoised_patches, denoised_patch_numbers = extract_y_channel_from_yuv_with_patch_numbers(denoised_path, row['width'], row['height'])
+
+        all_original_patches.extend(original_patches)
+        all_denoised_patches.extend(denoised_patches)
+        denoised_image_names.extend([row['original_image_name']] * len(denoised_patches))
+        all_patch_numbers.extend(denoised_patch_numbers)
+
+        scores = np.array([0 if float(score) == 0 else 1 for score in row['patch_score'].split(',')])
+        if len(scores) != len(original_patches) or len(scores) != len(denoised_patches):
+            print(f"Error: Mismatch in number of patches and scores for {row['original_image_name']}")
+            continue
+        all_scores.extend(scores)
+
+    return all_original_patches, all_denoised_patches, all_scores, denoised_image_names, all_patch_numbers
+
+def calculate_difference(original, ghosting):
+    return [ghost.astype(np.int16) - orig.astype(np.int16) for orig, ghost in zip(original, ghosting)]
+
+def prepare_data(data, labels):
+    data = np.array(data).astype('float32') / 255.0
+    lbl = np.array(labels)
+    return data, lbl
+
+def augmented_images(data, num_augmented_images_per_original):
+    augmented_images = []
+    
+    data_augmentation = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        vertical_flip=True,
+        fill_mode='nearest'
+    )
+
+    for i, patch in enumerate(data):
+        patch = np.expand_dims(patch, axis=0)
+        temp_generator = data_augmentation.flow(patch, batch_size=1)
+        
+        for _ in range(num_augmented_images_per_original):
+            augmented_image = next(temp_generator)[0]
+            augmented_image = np.squeeze(augmented_image)
+            augmented_images.append(augmented_image)
+    return augmented_images
+
+original_patches, denoised_patches, labels, denoised_image_names, all_patch_numbers = load_data_from_csv(csv_path, original_dir, denoised_dir)
+
+diff_patches = calculate_difference(original_patches, denoised_patches)
+diff_patches_np, labels_np = prepare_data(diff_patches, labels)
+
+combined = list(zip(diff_patches_np, labels_np, denoised_image_names, all_patch_numbers))
+combined = sklearn_shuffle(combined)
+
+ghosting_artifacts = [item for item in combined if item[1] == 1]
+non_ghosting_artifacts = [item for item in combined if item[1] == 0]
+
+num_ghosting_artifacts = len(ghosting_artifacts)
+num_non_ghosting_artifacts = len(non_ghosting_artifacts)
+
+print(f" Total GA Patches: {num_ghosting_artifacts}")
+print(f" Total NGA Labels: {num_non_ghosting_artifacts}")
+
+num_test_ghosting = 1500
+num_test_non_ghosting = 1500
+
+num_train_ghosting = num_ghosting_artifacts - num_test_ghosting
+num_train_non_ghosting = num_non_ghosting_artifacts - num_test_non_ghosting
+
+train_ghosting = ghosting_artifacts[num_test_ghosting:]
+test_ghosting = ghosting_artifacts[:num_test_ghosting]
+
+train_non_ghosting = non_ghosting_artifacts[num_test_non_ghosting:]
+test_non_ghosting = non_ghosting_artifacts[:num_test_non_ghosting]
+
+train_dataset = train_ghosting + train_non_ghosting
+test_dataset = test_ghosting + test_non_ghosting
+
+train_patches, train_labels, train_image_names, train_patch_numbers = zip(*train_dataset)
+test_patches, test_labels, test_image_names, test_patch_numbers = zip(*test_dataset)
+
+train_patches = np.array(train_patches)
+train_labels = np.array(train_labels)
+
+print(f" Total Train Patches: {len(train_patches)}")
+print(f" Total Train Labels: {len(train_labels)}")
+
+test_patches = np.array(test_patches)
+test_labels = np.array(test_labels)
+
+print(f" Total Test Patches: {len(test_patches)}")
+print(f" Total Test Labels: {len(test_labels)}")
+
+ghosting_patches = train_patches[train_labels == 1]
+
+ghosting_patches_expanded = np.expand_dims(ghosting_patches, axis=-1)
+augmented_images_np = augmented_images(ghosting_patches_expanded, num_augmented_images_per_original=12)
+
+augmented_images_np = np.stack(augmented_images_np)
+augmented_labels = np.ones(len(augmented_images_np))
+
+train_patches_expanded = np.expand_dims(train_patches, axis=-1)
+augmented_images_np_expanded = np.expand_dims(augmented_images_np, axis=-1)
+
+train_patches_combined = np.concatenate((train_patches_expanded, augmented_images_np_expanded), axis=0)
+train_labels_combined = np.concatenate((train_labels, augmented_labels), axis=0)
+
+print(f" Total Augmented Patches: {len(train_patches_combined)}")
+aghosting_patches = train_patches_combined[train_labels_combined == 1]
+print(f" Total Augmented GA: {len(aghosting_patches)}")
+
+X_train, X_test, y_train, y_test = train_test_split(train_patches_combined, train_labels_combined, test_size=0.15, random_state=42)
+
+y_train = tf.keras.utils.to_categorical(y_train, 2)
+y_test = tf.keras.utils.to_categorical(y_test, 2)
+
+print(f"X_Train Shape: {X_train.shape}")
+print(f"y_Train Shape: {y_train.shape}")
+print(f"X_Test Shape: {X_test.shape}")
+print(f"y_Test Shape: {y_test.shape}")
+
+# Implement NAS using AutoKeras
+clf = ak.ImageClassifier(
+    overwrite=True,
+    max_trials=10,
+    objective='val_accuracy'
+)
+
+# Train the model
+clf.fit(X_train, y_train, epochs=2, validation_data=(X_test, y_test))
+
+# Evaluate the model
+loss, accuracy = clf.evaluate(X_test, y_test)
+print(f"Test accuracy: {accuracy}")
+
+# Predict on new data
+predicted_y = clf.predict(X_test)
+
+# Retrieve the best model
+best_model = clf.export_model()
+
+# Save the best model
+# best_model.save("best_model_autokeras")
+
+# Summary of the best model
+best_model.summary()
