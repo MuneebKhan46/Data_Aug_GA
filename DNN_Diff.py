3d0433eb8a49aa611510e2dc662c186bf8b3b91a
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-05-16 03:00:57 +0900

    Update DNN_Diff.py

--- DNN_Diff.py
+++ DNN_Diff.py
@@ -27,8 +27,8 @@ class_1_accuracies = []
 
 original_dir = '/Dataset/dataset_patch_raw_ver3/original'
 denoised_dir = '/Dataset/dataset_patch_raw_ver3/denoised'
-csv_path     = '/Dataset/Data_Aug_GA/patch_label_median_verified2.csv'
-result_file_path = "/Dataset/Results/New_Overall_results.csv"
+csv_path     = '/Dataset/Data_Aug_GA/patch_label_median_verified3.csv'
+result_file_path = "/Dataset/Results/Overall_results.csv"
 
 
 def extract_y_channel_from_yuv_with_patch_numbers(yuv_file_path: str, width: int, height: int):
@@ -286,7 +286,7 @@ dnn_wcw_model = create_dnn_model()
 dnn_wcw_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-wcw_model_checkpoint = keras.callbacks.ModelCheckpoint(filepath='/Dataset/new_Model/DNN_Diff_wCW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
+wcw_model_checkpoint = keras.callbacks.ModelCheckpoint(filepath='/Dataset/Model/DNN_Diff_wCW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
 wcw_history = dnn_wcw_model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test), callbacks=[wcw_model_checkpoint])
 memMb_vgg19 =resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
 print("%5.1f MByte" % memMb_vgg19)
@@ -310,7 +310,7 @@ dnn_cw_model = create_dnn_model()
 dnn_cw_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-cw_model_checkpoint = ModelCheckpoint(filepath='/Dataset/new_Model/DNN_Diff_CW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
+cw_model_checkpoint = ModelCheckpoint(filepath='/Dataset/Model/DNN_Diff_CW.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
 cw_history = dnn_cw_model.fit(X_train, y_train, epochs=20, class_weight=class_weight, validation_data=(X_test, y_test), callbacks=[cw_model_checkpoint])
 memMb_vgg19 =resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
 print("%5.1f MByte" % memMb_vgg19)
@@ -355,7 +355,7 @@ dnn_cb_model = create_dnn_model()
 dnn_cb_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-cb_model_checkpoint = ModelCheckpoint(filepath='/Dataset/new_Model/DNN_Diff_CB.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
+cb_model_checkpoint = ModelCheckpoint(filepath='/Dataset/Model/DNN_Diff_CB.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
 cb_history = dnn_cb_model.fit(cb_train_patches, cb_train_labels, epochs=20, class_weight=class_weight, validation_data=(cb_test_patches, cb_test_labels), callbacks=[cb_model_checkpoint])
 memMb_vgg19 =resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
 print("%5.1f MByte" % memMb_vgg19)
@@ -380,7 +380,7 @@ true_labels = np.argmax(test_labels, axis=-1)
 
 report = classification_report(true_labels, predicted_labels, output_dict=True, target_names=["Non-Ghosting Artifact", "Ghosting Artifact"])
 
-misclass_wCW_csv_path = '/Dataset/New_CSV/DNN_Diff_wCW_misclassified_patches.csv'
+misclass_wCW_csv_path = '/Dataset/CSV/DNN_Diff_wCW_misclassified_patches.csv'
 misclassified_indexes = np.where(predicted_labels != true_labels)[0]
 misclassified_data = []
 
@@ -460,7 +460,7 @@ true_labels = np.argmax(test_labels, axis=-1)
 
 report = classification_report(true_labels, predicted_labels, output_dict=True, target_names=["Non-Ghosting Artifact", "Ghosting Artifact"])
 
-misclass_CW_csv_path  = '/Dataset/New_CSV/DNN_Diff_CW_misclassified_patches.csv'    
+misclass_CW_csv_path  = '/Dataset/CSV/DNN_Diff_CW_misclassified_patches.csv'    
 
 misclassified_indexes = np.where(predicted_labels != true_labels)[0]
 misclassified_data = []
@@ -540,7 +540,7 @@ true_labels = np.argmax(test_labels, axis=-1)
 
 report = classification_report(true_labels, predicted_labels, output_dict=True, target_names=["Non-Ghosting Artifact", "Ghosting Artifact"])
 
-misclass_CB_csv_path  = '/Dataset/New_CSV/DNN_Diff_CB_misclassified_patches.csv'    
+misclass_CB_csv_path  = '/Dataset/CSV/DNN_Diff_CB_misclassified_patches.csv'    
 misclassified_indexes = np.where(predicted_labels != true_labels)[0]
 misclassified_data = []
 
@@ -655,7 +655,7 @@ print(f"Accuracy: {test_acc:.4f} | precision: {weighted_precision:.4f}, Recall={
 
 
 
-misclass_En_csv_path = '/Dataset/New_CSV/Ensemble_DNN_Diff_misclassified_patches.csv'
+misclass_En_csv_path = '/Dataset/CSV/Ensemble_DNN_Diff_misclassified_patches.csv'
 
 misclassified_indexes = np.where(predicted_classes != true_labels)[0]
 misclassified_data = []
